39045d4422e81018d3a536e4871b516b75c04d1a
Collappo <collappo@ogarnij.se>
2026-04-11 17:51:33 +0200

    Added incoming birthdays info

--- main.py
+++ main.py
@@ -31,29 +31,32 @@ def main(screen):
     curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_WHITE) # for selected item
     curses.init_pair(3, curses.COLOR_CYAN, curses.COLOR_BLACK) # for [ ], age
     curses.init_pair(4, curses.COLOR_MAGENTA, curses.COLOR_BLACK) # for >, hotkeys
-    curses.init_pair(5, curses.COLOR_YELLOW, curses.COLOR_BLACK) # for details
+    curses.init_pair(5, curses.COLOR_YELLOW, curses.COLOR_BLACK) # for incoming birthdays
     curses.init_pair(6, curses.COLOR_RED, curses.COLOR_BLACK) # for exit hotkey
+    curses.init_pair(7, curses.COLOR_GREEN, curses.COLOR_BLACK) # for birthdays
     
     # Initialize data variables
     base_x = 4
-    base_y = 3
+    base_y = 4
     index = 0
     
     curses.curs_set(0)
     while True:
         # Getting data
         data = [x for x in pd.read_csv(r"storage/data.csv").values]
+
         max_spaces = max(len(f"{item[0]}") for item in data)
         max_spaces += 2
-        max_age_chars = len(str(len(data) - 1))
+        # max_age_chars = len(str(len(data) - 1)) + len(" -> ")
+        max_age_chars = max(len(f"{(datetime.now() - datetime.strptime(item[1], "%d.%m.%Y")).days // 365}") for item in data)
     
         # Drawing UI
         screen.border()
         draw_text_logo(screen)
         
-        screen.addstr(base_y - 1, base_x + max_age_chars, "Age", curses.color_pair(3))
-        screen.addstr(base_y - 1, base_x + max_age_chars + 6, "Name", curses.color_pair(1))
-        screen.addstr(base_y - 1, base_x + max_age_chars + 7 + max_spaces, "Birthdate", curses.color_pair(5))
+        screen.addstr(base_y - 2, base_x + max_age_chars, "Age", curses.color_pair(3))
+        screen.addstr(base_y - 2, base_x + max_age_chars + 6, "Name", curses.color_pair(1))
+        screen.addstr(base_y - 2, base_x + max_age_chars + 7 + max_spaces, "Birthdate", curses.color_pair(5))
         
         for id, row in enumerate(data):
             x = base_x + max_age_chars + 5
@@ -61,18 +64,23 @@ def main(screen):
             name = row[0]
             birthdate = row[1]
             age = (datetime.now() - datetime.strptime(birthdate, "%d.%m.%Y")).days // 365
-            next_age = age + 1
-            age_str = f"{age}"
-                        
+            left_days = (datetime.strptime(birthdate[:-4] + str(datetime.now().year), "%d.%m.%Y") - datetime.strptime(f"{datetime.now().day}.{datetime.now().month}.{datetime.now().year}", "%d.%m.%Y")).days
+            
+            color_for_birthdate = 1
+            if left_days <= 30 and left_days > 0:
+                color_for_birthdate = 5
+            elif left_days == 0:
+                color_for_birthdate = 7
+                                    
             if id == index:
-                screen.addstr(base_y + id, x - max_age_chars - 6, ">", curses.color_pair(4))
+                screen.addstr(base_y + id, x - max_age_chars - 4, ">", curses.color_pair(4))
                 screen.addstr(base_y + id, x - 2 - max_age_chars + (max_age_chars - len(str(age))), str(age), curses.color_pair(3))
                 screen.addstr(base_y + id, x, ' ' + name + ' ', curses.color_pair(2))
-                screen.addstr(base_y + id, x + len(name) + (max_spaces - len(name)) + 2, birthdate, curses.color_pair(1))
+                screen.addstr(base_y + id, x + len(name) + (max_spaces - len(name)) + 2, birthdate, curses.color_pair(color_for_birthdate))
             else:
                 screen.addstr(base_y + id, x - 2 - max_age_chars + (max_age_chars - len(str(age))), str(age), curses.color_pair(3))
                 screen.addstr(base_y + id, x, ' ' + name + ' ', curses.color_pair(1))
-                screen.addstr(base_y + id, x + len(name) + (max_spaces - len(name)) + 2, birthdate, curses.color_pair(1))
+                screen.addstr(base_y + id, x + len(name) + (max_spaces - len(name)) + 2, birthdate, curses.color_pair(color_for_birthdate))
         
         draw_hotkeys(screen)
         
