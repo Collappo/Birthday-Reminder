26400383ee49eccc5b31dbd3e5970cf7494f2254
Collappo <collappo@ogarnij.se>
2026-04-11 02:32:32 +0200

    Added hotkeys

--- main.py
+++ main.py
@@ -15,14 +15,24 @@ def draw_text_logo(screen):
     screen.addstr(0, 4, "Birthday Reminder", curses.color_pair(1))
     screen.addstr(0, 2 + len(" [") + len("Birthday Reminder"), "] ", curses.color_pair(3))
     
+def draw_hotkeys(screen):
+    h, w = screen.getmaxyx()
+    
+    # Functional hotkeys
+    screen.addstr(h - 3, 2, "Navigate: Up/Down", curses.color_pair(4))
+    screen.addstr(h - 3, 22, "Edit: Enter", curses.color_pair(4))
+
+    # Exit hotkey
+    screen.addstr(h - 2, 2, "Exit: Alt + X", curses.color_pair(6))
 
 def main(screen):
     # Initialize color pairs
     curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK) # for normal item
     curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_WHITE) # for selected item
-    curses.init_pair(3, curses.COLOR_CYAN, curses.COLOR_BLACK) # for [ ]
-    curses.init_pair(4, curses.COLOR_MAGENTA, curses.COLOR_BLACK) # for >
+    curses.init_pair(3, curses.COLOR_CYAN, curses.COLOR_BLACK) # for [ ], age
+    curses.init_pair(4, curses.COLOR_MAGENTA, curses.COLOR_BLACK) # for >, hotkeys
     curses.init_pair(5, curses.COLOR_YELLOW, curses.COLOR_BLACK) # for details
+    curses.init_pair(6, curses.COLOR_RED, curses.COLOR_BLACK) # for exit hotkey
     
     # Initialize data variables
     base_x = 4
@@ -41,8 +51,9 @@ def main(screen):
         screen.border()
         draw_text_logo(screen)
         
-        row = f"Age    Name    Birthdate"
-        screen.addstr(base_y - 1, base_x, row, curses.color_pair(1))
+        screen.addstr(base_y - 1, base_x + max_age_chars, "Age", curses.color_pair(3))
+        screen.addstr(base_y - 1, base_x + max_age_chars + 6, "Name", curses.color_pair(1))
+        screen.addstr(base_y - 1, base_x + max_age_chars + 7 + max_spaces, "Birthdate", curses.color_pair(5))
         
         for id, row in enumerate(data):
             x = base_x + max_age_chars + 5
@@ -62,6 +73,8 @@ def main(screen):
                 screen.addstr(base_y + id, x - 2 - max_age_chars + (max_age_chars - len(str(age))), str(age), curses.color_pair(3))
                 screen.addstr(base_y + id, x, ' ' + name + ' ', curses.color_pair(1))
                 screen.addstr(base_y + id, x + len(name) + (max_spaces - len(name)) + 2, birthdate, curses.color_pair(1))
+        
+        draw_hotkeys(screen)
         
         # Getting clicked key
         key = screen.getch()
